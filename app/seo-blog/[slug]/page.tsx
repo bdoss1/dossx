f3321a8ce803f932ce4d85ec47bd5a136dc46b5d
Baron Doss <barondoss@Barons-MacBook-Pro.local>
2025-07-18 16:00:49 -0500

    updated blog link

--- app/seo-blog/[slug]/page.tsx
+++ app/seo-blog/[slug]/page.tsx
@@ -17,7 +17,7 @@ export async function generateStaticParams() {
 
 const BlogDetails = async ({ params }: { params: Promise<{ slug: string }> }) => {
   const slug = (await params).slug
-  const blog = getMarkDownContent('data/blogs/', slug)
+  const blog = getMarkDownContent('data/blogsV2/', slug)
   const postBlog = blog.data
 
   return (
@@ -31,14 +31,6 @@ const BlogDetails = async ({ params }: { params: Promise<{ slug: string }> }) =>
       <BlogContent blog={blog} />
       <CTA>
         Let's chat!
-        <CtaImageSlider
-          slides={[
-            { id: '1', img: '/images/agent/18.png' },
-            { id: '2', img: '/images/agent/19.png' },
-            { id: '3', img: '/images/agent/03.jpg' },
-          ]}
-        />
-        with us.
         <i className="block font-instrument italic max-md:inline-block max-sm:pl-2 sm:mt-10">A virtual coffee?</i>
       </CTA>
     </LayoutOne>
