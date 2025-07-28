e3dd449f5c72e9a8f73df61c7b0e52308dd5bafd
Baron Doss <barondoss@Mac.login.dynalink>
2025-07-28 10:43:01 -0500

    updated FAQ

--- app/faq/page.tsx
+++ app/faq/page.tsx
@@ -13,16 +13,7 @@ const FAQPage = () => {
       <FAQ bigTitleWithBadge={true} />
       <CTA>
         Let's chat!
-        <CtaImageSlider
-          slides={[
-            { id: '1', img: '/images/agent/11.png' },
-            { id: '2', img: '/images/agent/01.jpg' },
-            { id: '3', img: '/images/agent/12.png' },
-          ]}
-        />
-        with us.
-        <i className="block font-instrument italic max-md:inline-block max-sm:pl-2 sm:mt-10">A virtual coffee?</i>
-      </CTA>
+       </CTA>
     </LayoutOne>
   )
 }
